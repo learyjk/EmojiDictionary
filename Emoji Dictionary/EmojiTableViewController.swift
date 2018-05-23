0125ce8f65a86740e9e472b2db988b4609bb0fd7
Unknown <leary.keegan@gmail.com>
2018-05-23 18:48:40 -0400

    Created Tableview and Seque is working

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -10,22 +10,27 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
+    var emojis = ["😀","💩","🏎","💒","🍎"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return emojis.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "carSegue", sender: nil)
     }
 
 }
